ca406fc517733310543dfd3fd7b3b49df7e4fcc0
RossKM <54750763+RossKM@users.noreply.github.com>
2022-03-23 09:19:40 +0000

    Update rsync.sh
    Error code improvements

--- rsync.sh
+++ rsync.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
-# Use -n to perform a dry run.
-# This is a simple script I use to Rsync between my desktop and laptop.
+# Use -n to perform a dry run of rsync.
 
-DIRS="Documents Pictures .utils "
-CONFIG="~/.config"
-RECEIVER="laptop" #Name of device in /etc/hosts or IP.
+cd
+DIRS="Documents Pictures .utils"
+CONFIG=".config"
+declare -i e=0
+DEST="arch" #name from /etc/hosts or local IP address.
 
-# Sync the main dirs
-rsync -uavrP --bwlimit=4000 --no-relative --delete-after $DIRS $RECEIVER: 
-if [[ $? -eq 0 ]]; then
-	notify-send "Main Directory sync complete."
+# Sync the main directories
+rsync -uavrP --bwlimit=4000 --no-relative --delete-after /home/ross/$DIRS $DEST: 
+while [[ $? -gt 0 ]]; do
+	e=$((e+1)) # Produces an error code if the rsync line produces an error.
+	echo "Error"
+done
+
+# Config and misc directories
+rsync -uavrP --bwlimit=4000 --no-relative --delete-after $CONFIG/vim $CONFIG/i3 $DEST:~/.config
+while [[ $? -gt 0 ]]; do
+	e=$((e+1))
+	echo "Error."
+done
+# Aiming to do both of these in the same loop down the road.
+
+echo "$e"
+if [["$e" -gt 0]]; then # If errors codes are more than 0, there was an error somewhere.
+	notify-send "There were error(s) with sync."
 else
-	notify-send "Sync Error"
+	notify-send "Sync successful."
 fi
-# Config and other dirs
-rsync -uavrP --bwlimit=4000 --no-relative --delete-after $CONFIG/vim $CONFIG/i3 $RECEIVER:
-if [[ $? -eq 0 ]]; then
-	notify-send "Config Directory sync complete."
-else
-	notify-send "Sync Error"
-fi
+cd -
